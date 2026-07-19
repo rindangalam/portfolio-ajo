796299612e237ec31d266c26f05497cef0a27c0e
Rindang Alam Nur Muhammad <rindangalam04@gmail.com>
2026-07-19 08:11:27 +0700

    feat: add revalidation for sitemap and create robots.txt for SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,6 +3,8 @@ import { createClient } from "@/lib/supabase/server";
 
 const BASE_URL = "https://rindang-alam.vercel.app";
 
+export const revalidate = 3600;
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const supabase = await createClient();
 
